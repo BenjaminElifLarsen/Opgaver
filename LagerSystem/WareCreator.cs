e985c3ce784445d6313f08df525ff8ba51479690
Benjamin Larsen <beela93@hotmail.com>
2020-08-19 16:29:20 +0200

    minor work

--- LagerSystem/WareCreator.cs
+++ LagerSystem/WareCreator.cs
@@ -16,9 +16,12 @@ namespace LagerSystem
             this.warePublisher = warePublisher;
         }
 
-        public void CreateWare()
+        public void CreateWare() //at some point, either after all information has been added or after each, ask if it/they is/are correct and if they want to reenter information.
         {
-
+            string ID = CreateID();
+            string name = "";
+            uint amount = 0;
+            Publisher.PubWare.CreateWare(name,ID,amount);
         }
 
         public string CreateID()
@@ -38,7 +41,7 @@ namespace LagerSystem
         {
             Type type = Type.GetType(e.Type);
             if (type == Type.GetType("Liquids"))
-                WareInformation.Ware.Add(new Liquids(e.Name, e.ID, e.Amount, warePublisher)); //needs to deal with different types, maybe just use polymorphy 
+                WareInformation.Ware.Add(new Liquids(e.Name, e.ID, e.Amount, warePublisher)); //needs to deal with different types, maybe just use polymorphy or a combination?
         }
 
         public void RemoveFromSubscription(WarePublisher warePublisher)
