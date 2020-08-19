14ae9015483e53f4837f8beb553b27455dac76cf
Benjamin Larsen <beela93@hotmail.com>
2020-08-19 10:19:58 +0200

    more clean code and some bug fixing.

--- Reaktions spil/Program.cs
+++ Reaktions spil/Program.cs
@@ -8,6 +8,7 @@ namespace Reaktions_spil
         static void Main(string[] args)
         {
             Game game = new Game();
+            game.Menu();
         }
     }
 
@@ -16,21 +17,26 @@ namespace Reaktions_spil
     /// </summary>
     public class Timer
     {
-        private static DateTime time = new DateTime();
-        private static DateTime startTime;
-        Timer()
-        {
+        private DateTime startTime;
 
-        }
-
-        public static DateTime Time { get => time; }
-
-        public static void SetTime()
+        /// <summary>
+        /// Sets the time to the current system time.
+        /// </summary>
+        public void SetTime()
         {
             startTime = DateTime.Now;
         }
 
-        public static double TimePassed { get => (DateTime.Now - startTime).TotalMilliseconds; }
+        /// <summary>
+        /// Calculates and returns the amount of miliseconds that have passed since <c>SetTime()</c> was called. If <c>SetTime()</c> was not called before it will throw a null reference exception.
+        /// </summary>
+        public double TimePassed { get
+            {
+                if (startTime.Year == 1)
+                    throw new TimeNotSetException("SetTime has not been called");
+                return (DateTime.Now - startTime).TotalMilliseconds;
+            }
+        }
     }
 
     /// <summary>
@@ -38,62 +44,52 @@ namespace Reaktions_spil
     /// </summary>
     public class Game
     {
-
-        private static List<double> highscores = new List<double>();
-        private double countup = 0; //miliseconds
+        private List<double> highscores = new List<double>();
         private const ConsoleKey gameStart = ConsoleKey.W;
         private ConsoleKey player1ReactionKey;
         private ConsoleKey player2ReactionKey;
-        private static Random rnd = new Random();
+        private Random rnd = new Random();
         private const char sign = 'O';
-
-        /// <summary>
-        /// Basic constructor. 
-        /// </summary>
-        public Game()
-        {
-            Menu();
-        }
 
         /// <summary>
         /// The game main menu. 
         /// </summary>
         public void Menu()
         {
-            const ConsoleKey singleplayer = ConsoleKey.D1;
-            const ConsoleKey multiplayer = ConsoleKey.D2;
-            const ConsoleKey exit = ConsoleKey.D4;
+            const ConsoleKey singleplayerKey = ConsoleKey.D1;
+            const ConsoleKey multiplayerKey = ConsoleKey.D2;
+            const ConsoleKey exitKey = ConsoleKey.D4;
             const ConsoleKey highscoreKey = ConsoleKey.D3;
-            ConsoleKey pressedKey = ConsoleKey.D5;
+            char singleplayerControlChar = singleplayerKey.ToString()[singleplayerKey.ToString().Length - 1];
+            char multiplayerControlChar = multiplayerKey.ToString()[multiplayerKey.ToString().Length - 1];
+            char exitControlChar = highscoreKey.ToString()[highscoreKey.ToString().Length - 1];
+            char highscoreControlChar = exitKey.ToString()[exitKey.ToString().Length - 1];
+            ConsoleKey pressedKey;
             bool selected = false;
             while (true) {
                 Console.Clear();
-                Console.WriteLine("Press {0} for singleplayer. Press {1} for multiplayer. Press {2} for highscore. Press {3} to shutdown.", 
-                    singleplayer.ToString()[singleplayer.ToString().Length-1], multiplayer.ToString()[multiplayer.ToString().Length - 1], highscoreKey.ToString()[highscoreKey.ToString().Length - 1], exit.ToString()[exit.ToString().Length - 1]);
+                PrintOutMessage(String.Format("Press {0} for singleplayer. Press {1} for multiplayer. Press {2} for highscore. Press {3} to shutdown.",
+                    singleplayerControlChar, multiplayerControlChar, exitControlChar, highscoreControlChar));
                 do
                 {
                     while (!Console.KeyAvailable) ;
                     pressedKey = Console.ReadKey(true).Key;
-                    if(pressedKey == singleplayer || pressedKey == multiplayer || pressedKey == exit || pressedKey == highscoreKey)
+                    if(pressedKey == singleplayerKey || pressedKey == multiplayerKey || pressedKey == exitKey || pressedKey == highscoreKey)
                             selected = true;
-
-                    while (Console.KeyAvailable)
-                        Console.ReadKey(true); //flush the buffer
-
+                    BufferFlush();
                 } while (!selected);
                 selected = false;
                 switch (pressedKey)
                 {
-                    case singleplayer:
-                        SingleRun();
+                    case singleplayerKey:
+                        SinglePlayerRun();
                         break;
 
-                    case multiplayer:
-                        MultiRun();
+                    case multiplayerKey:
+                        MultiPlayerRun();
                         break;
 
-
-                    case exit:
+                    case exitKey:
                         Environment.Exit(0);
                         break;
 
@@ -112,9 +108,9 @@ namespace Reaktions_spil
             Console.Clear();
             if (highscores.Count != 0)
                 foreach (double score in highscores)
-                    Console.WriteLine(score);
+                    PrintOutMessage(score.ToString());
             else
-                Console.WriteLine("No highscore yet.");
+                PrintOutMessage("No highscore yet.");
             Wait();
         }
 
@@ -123,8 +119,8 @@ namespace Reaktions_spil
         /// </summary>
         private void Wait()
         {
-            Console.WriteLine("Press any key to continue.");
-            Console.ReadKey();
+            PrintOutMessage("Press any key to continue.");
+            Console.ReadKey(true);
         }
 
         /// <summary>
@@ -133,122 +129,189 @@ namespace Reaktions_spil
         private void BufferFlush()
         {
             while (Console.KeyAvailable)
-                Console.ReadKey(true); //flush the buffer
+                Console.ReadKey(true); 
         }
 
         /// <summary>
-        /// Multipleplayer game loop.
+        /// Runs the multiplayer.
         /// </summary>
-        private void MultiRun()
+        private void MultiPlayerRun()
         {
             Console.Clear();
-            bool pressedStartKey = false;
+            SetMultiPlayerKeys();
+            Console.WriteLine("Press {0} to start. {1}Player 1: Pres {2} when you see {4}. {1}Player 2: Pres {3} when you see {4}.", 
+                gameStart, Environment.NewLine, player1ReactionKey, player2ReactionKey, sign);
+            MultipleGameLoop();
+            UnsetMultiPlayerKeys();
+            Wait();
+        }
+
+        /// <summary>
+        /// Sets the player keys for multiplayer. 
+        /// </summary>
+        private void SetMultiPlayerKeys()
+        {
             player1ReactionKey = ConsoleKey.Spacebar;
             player2ReactionKey = ConsoleKey.Enter;
-            bool player1Pressed = false;
-            bool player2Pressed = false;
+        }
 
-            Console.WriteLine("Press {0} to start. {1}Player 1: Pres {2} when you see {4}. {1}Player 2: Pres {3} when you see {4}."
-                , gameStart, Environment.NewLine, player1ReactionKey, player2ReactionKey, sign);
+        /// <summary>
+        /// Unsets the player keys for multiplayer
+        /// </summary>
+        private void UnsetMultiPlayerKeys()
+        {
+            player1ReactionKey = 0;
+            player2ReactionKey = 0;
+        }
 
+        /// <summary>
+        /// Contains the game loop for the multiplayer part.
+        /// </summary>
+        private void MultipleGameLoop()
+        {
+
+            bool pressedStartKey = false;
             do
             {
-
                 while (!Console.KeyAvailable) ;
-                if (Console.ReadKey(true).Key == gameStart) //delegate/event this
+                if (Console.ReadKey(true).Key == gameStart)
                 {
-                    pressedStartKey = true;
-                    Timer.SetTime();
+                    bool player1Pressed = false;
+                    bool player2Pressed = false;
                     double player1Timer = 0;
                     double player2Timer = 0;
-                    float timeToCountDown = (rnd.Next(1, 8) + (rnd.Next(0, 1000) / 1000f)) * 1000;
-                    countup = 0;
-                    while (countup < timeToCountDown)
-                    {
-                        countup = Timer.TimePassed;
-                        BufferFlush();
-                    }
-                    Console.WriteLine(sign);
-                    Timer.SetTime();
+                    pressedStartKey = true;
+
+                    PrintOutMessage("Game Started");
+                    CountUp();
+                    PrintOutMessage(sign.ToString(), ConsoleColor.Green);
+
+                    Timer timer = new Timer();
+                    timer.SetTime();
                     do
                     {
-                        while (!Console.KeyAvailable) ; 
-                        ConsoleKey pressedKey = Console.ReadKey(true).Key;
-                        if(pressedKey == player1ReactionKey && !player1Pressed)
+                        ConsoleKey pressedKey = KeyGet();
+                        if (pressedKey == player1ReactionKey && !player1Pressed)
                         {
                             player1Pressed = true;
-                            player1Timer = Timer.TimePassed;
-                        }else if(pressedKey == player2ReactionKey && !player2Pressed)
+                            player1Timer = timer.TimePassed;
+                        }
+                        else if (pressedKey == player2ReactionKey && !player2Pressed)
                         {
                             player2Pressed = true;
-                            player2Timer = Timer.TimePassed;
+                            player2Timer = timer.TimePassed;
                         }
-
                     } while (!player2Pressed || !player1Pressed);
-                    Console.WriteLine("Player 1 time spent: {0} ms. {2}Player 2 time spent: {1} ms.", player1Timer, player2Timer, Environment.NewLine);
-                    string winner = player1Timer < player2Timer ? "Payer 1": "Player 2";
-                    Console.WriteLine("{0} won", winner);
+                    PrintOutMessage(String.Format("Player 1 time spent: {0} ms. {2}Player 2 time spent: {1} ms.", player1Timer, player2Timer, Environment.NewLine));
+                    PrintOutMessage(String.Format("{0} won", player1Timer < player2Timer ? "Payer 1" : "Player 2"), ConsoleColor.Red);
+                    AddHighScore(player1Timer);
+                    AddHighScore(player2Timer);
                 }
-
-
                 BufferFlush();
-
             } while (!pressedStartKey);
+        }
+
+        /// <summary>
+        /// Prints out <paramref name="message"/> in the <paramref name="colour"/>.
+        /// </summary>
+        /// <param name="message">Message to print out</param>
+        /// <param name="colour">The colour to print the <paramref name="message"/> in.</param>
+        private void PrintOutMessage(string message, ConsoleColor colour = ConsoleColor.White)
+        {
+            Console.ForegroundColor = colour;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        /// <summary>
+        /// Waits on a key to be pressed and then returns the key without displaying it.
+        /// </summary>
+        /// <returns>Returns the pressed key without displaying it.</returns>
+        private ConsoleKey KeyGet()
+        {
+            while (!Console.KeyAvailable) ;
+            return Console.ReadKey(true).Key;
+        }
+
+        /// <summary>
+        /// Counts up to a random time period from 1000 and up to 7999 miliseconds.  
+        /// </summary>
+        private void CountUp()
+        {
+            Timer timer = new Timer();
+            float timeToCountDown = (rnd.Next(1, 7)*1000 + (rnd.Next(0, 1999)));
+            double countup = 0;
+            timer.SetTime();
+            while (countup < timeToCountDown)
+            {
+                countup = timer.TimePassed;
+                BufferFlush();
+            }
+        }
+
+        /// <summary>
+        /// Runs the singleplayer.
+        /// </summary>
+        private void SinglePlayerRun()
+        {
+            Console.Clear();
+            SetSinglePlayerKey();
+            PrintOutMessage(String.Format("Press {0} to start. {1}Pres {2} when you see {3}", gameStart, Environment.NewLine, player1ReactionKey, sign));
+            SinglePlayerGameLoop();
+            UnsetSinglePlayerKey();
             Wait();
         }
 
         /// <summary>
-        /// Singleplayer game loop.
+        /// Sets the single player key.
         /// </summary>
-        private void SingleRun()
+        private void SetSinglePlayerKey()
         {
-            Console.Clear();
             player1ReactionKey = ConsoleKey.Enter;
-            bool pressedStartKey = false;
-            Console.WriteLine("Press {0} to start. {1}Pres {2} when you see {3}", gameStart, Environment.NewLine, player1ReactionKey, sign);
-            do
-            {
-                
-                while (!Console.KeyAvailable) ;
-                if (Console.ReadKey(true).Key == gameStart) //delegate/event this
-                {
-                    pressedStartKey = true;
-                    Timer.SetTime();
-                    double playerTimer = 0;
-                    float timeToCountDown = (rnd.Next(1, 8) + (rnd.Next(0, 1000) / 1000f))*1000;
-                    countup = 0;
-                    while (countup < timeToCountDown)
-                    {
-                        countup = Timer.TimePassed;
-                        BufferFlush();
-                    }
-                    Console.WriteLine(sign);
-                    Timer.SetTime();
-                    while (!Console.KeyAvailable && Console.ReadKey(true).Key != player1ReactionKey) ;
-                    playerTimer = Timer.TimePassed;
-                    Console.WriteLine("Time spent: {0} ms.", playerTimer);
-                    AddHighScore(playerTimer);
-                }
-
-
-                BufferFlush();
-
-            } while (!pressedStartKey);
-            Wait();
-
         }
 
+        /// <summary>
+        /// Unsets the single player key.
+        /// </summary>
+        private void UnsetSinglePlayerKey()
+        {
+            player1ReactionKey = 0;
+        }
+
+        /// <summary>
+        /// Contains the gameloop for the singleplayer part. 
+        /// </summary>
+        private void SinglePlayerGameLoop()
+        {
+            bool pressedStartKey = false;
+            do
+            {
+                while (!Console.KeyAvailable) ;
+                if (Console.ReadKey(true).Key == gameStart)
+                {
+                    pressedStartKey = true;
+                    PrintOutMessage("Game Started");
+                    CountUp();
+                    PrintOutMessage(sign.ToString(), ConsoleColor.Green);
+                    Timer timer = new Timer();
+                    timer.SetTime();
+                    while (!Console.KeyAvailable && Console.ReadKey(true).Key != player1ReactionKey) ;
+                    double playerTimer = timer.TimePassed;
+                    PrintOutMessage(String.Format("Time spent: {0} ms.", playerTimer), ConsoleColor.Red);
+                    AddHighScore(playerTimer);
+                }
+                BufferFlush();
+            } while (!pressedStartKey);
+        }
 
         /// <summary>
         /// Adds <paramref name="time"/> to the highscore at the correct location. If the highscore is above ten the last value(s) is/are removed. 
         /// </summary>
-        /// <param name="time"></param>
-        private static void AddHighScore(double time)
+        /// <param name="time">The time to add to the highscore</param>
+        private void AddHighScore(double time)
         {
             if (highscores.Count == 0)
-            {
                 highscores.Add(time);
-            }
             else
             {
                 int highscoreCounter = highscores.Count;
@@ -257,7 +320,7 @@ namespace Reaktions_spil
                 {
                     if (pos < highscoreCounter && highscores[pos] > time)
                         highscores.Insert(pos, time);
-                    else if (pos == highscores.Count && highscoreCounter < 10)
+                    else if (pos == highscoreCounter && highscoreCounter < 10)
                         highscores.Add(time);
                     pos++;
                 }
@@ -269,4 +332,30 @@ namespace Reaktions_spil
         }
 
     }
+
+    /// <summary>
+    /// Custom exception used for the class <c>Timer</c>.
+    /// </summary>
+    class TimeNotSetException : Exception
+    {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public TimeNotSetException()
+        {
+
+        }
+
+        /// <summary>
+        /// Constructor that takes <paramref name="message"/>,
+        /// </summary>
+        /// <param name="message">Message of the error,</param>
+        public TimeNotSetException(string message) : base(message)
+        {
+
+        }
+
+
+    }
+
 }
