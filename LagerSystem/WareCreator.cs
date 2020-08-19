ce72b647bf19e5682928ee9d1f7ff3c5de3f238c
Benjamin Larsen <beela93@hotmail.com>
2020-08-19 16:56:34 +0200

    Added comment about ware creation idea and some functions for ware values

--- LagerSystem/WareCreator.cs
+++ LagerSystem/WareCreator.cs
@@ -17,27 +17,49 @@ namespace LagerSystem
         }
 
         public void CreateWare() //at some point, either after all information has been added or after each, ask if it/they is/are correct and if they want to reenter information.
-        {
+        { //have a creation menu where the user can select which entry they want to enter when they want to and they can first finish when all entries have been entered. Also add a "back" option
             string ID = CreateID();
-            string name = "";
-            uint amount = 0;
+            string name = EnterName();
+            uint amount = EnterAmount();
             Publisher.PubWare.CreateWare(name,ID,amount);
+        }
+
+        public string EnterName()
+        {
+            string name;
+            Console.WriteLine("Enter Product Name");
+            do
+            {
+                name = Console.ReadLine();
+            } while (name == null || name == "");
+            return name;
+        }
+
+        public uint EnterAmount()
+        {
+            uint value;
+            string valueString;
+            do
+            {
+                valueString = Console.ReadLine();
+            } while (uint.TryParse(valueString, out value));
+            return value;
         }
 
         public string CreateID()
         {
-            Console.WriteLine("Enter ID");
             string ID_;
+            Console.WriteLine("Enter Product ID");
             do
             {
                 ID_ = Console.ReadLine();
             } while (Support.UniqueID(ID_));
 
-            throw new NotImplementedException();
+            return ID_;
         }
 
 
-        protected void CreateWareEventHandler(object sender, ControlEvents.CreateWareEventArgs e)
+        protected void CreateWareEventHandler(object sender, ControlEvents.CreateWareEventArgs e) //instead of calling this here, call it in the main menu and change CreateWareEventArgs to not contain any parameters
         {
             Type type = Type.GetType(e.Type);
             if (type == Type.GetType("Liquids"))
