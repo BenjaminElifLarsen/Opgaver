089e0cee10cc61548d475efd9fa88118ebd3a599
Benjamin Larsen <beela93@hotmail.com>
2020-08-21 13:46:13 +0200

    Worked on Menu.WareChangeMenu() and added functions that it can call

--- LagerSystem/Menu.cs
+++ LagerSystem/Menu.cs
@@ -41,15 +41,54 @@ namespace LagerSystem
 
         private void WareChangeMenu()
         {
-            string[] options = new string[] { "Remove Ware", "Add To Ware", "Remove From Ware" };
+            string[] options = new string[] { "Remove Ware", "Add To Ware", "Remove From Ware", "Back" };
             Publisher.PubWare.RemoveFromWare("ID-55t",1);
             Publisher.PubWare.AddToWare("ID-55t2",1);
+            byte response = Visual.MenuRun(options);
+            switch (response)
+            {
+                case 0:
+                    WareRemoveMenu();
+                    break;
 
+                case 1:
+                    WareAddAmountMenu();
+                    break;
+
+                case 2:
+                    WareRemoveAmountMenu();
+                    break;
+            }
+        }
+
+        private void WareRemoveMenu()
+        {
+            WareModifier.RemoveWare(CollectID());
+        }
+
+        private void WareAddAmountMenu()
+        {
+            WareModifier.AddToWare(CollectID(),CollectAmount());
+        }
+
+        private void WareRemoveAmountMenu()
+        {
+            WareModifier.RemoveFromWare(CollectID(), CollectAmount());
+        }
+
+        private uint CollectAmount() //WareCreator.EnterAmount does what this should do, so consider moving the code of that function into a Support function and then call that function from here and WareCreator.EnterAmount
+        {
+            throw new NotImplementedException();
+        }
+
+        private string CollectID()
+        {
+            throw new NotImplementedException();
         }
 
         private void WareCreateMenu()
         {
-            WareCreator creator = new WareCreator(Publisher.PubWare);
+            WareCreator creator = new WareCreator(Publisher.PubWare); //move to somewhere else
             Publisher.PubWare.CreateWare();
         } 
 
