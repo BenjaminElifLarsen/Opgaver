50b18a99a2cb67de1076429835e4f61430d19a1a
Benjamin Larsen <beela93@hotmail.com>
2020-08-21 17:05:52 +0200

    Added an Electronic ware class and modified WareInformation to add it to the ware list.

---
+++ LagerSystem/Electronic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagerSystem
+{
+    [WareType("Electronic")]
+    sealed class Electronic : Ware
+    {
+        public Electronic(string name, string id, uint amount, WarePublisher warePublisher) : base(name, id, amount, warePublisher)
+        {
+        }
+    }
+}
