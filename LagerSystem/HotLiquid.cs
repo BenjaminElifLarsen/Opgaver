d517e067c2d4606dd26960a71bef63ee40f7e326
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-08-24 09:47:50 +0200

    added properties to HotLiquid

--- LagerSystem/HotLiquid.cs
+++ LagerSystem/HotLiquid.cs
@@ -9,10 +9,22 @@ namespace LagerSystem
     [WareType("HotLiquid")]
     class HotLiquid : Liquid
     {
+        private double minTemp;
+        private double maxTemp;
         public HotLiquid(string name, string id, int amount, WarePublisher warePublisher) : base(name, id, amount, warePublisher)
         {
         }
 
-        public double MinimumTemperatur { get; }
+        public HotLiquid(string name, string id, int amount, double minTemp, double maxTemp, WarePublisher warePublisher) : base(name, id, amount, warePublisher)
+        {
+            this.minTemp = minTemp;
+            this.maxTemp = maxTemp;
+        }
+
+        public double MinimumTemperatur { get => minTemp; }
+    
+        public double MaximumTemperatur { get => maxTemp; }
+
+
     }
 }
