9f8561ae6ad87b9d78a6cf193062d609ebbdf127
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-08-28 10:22:41 +0200

    minor work

--- DatabaseProgrammering/Webshop/Program.cs
+++ DatabaseProgrammering/Webshop/Program.cs
@@ -7,14 +7,29 @@ namespace Webshop
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
             SQLet.ConnectSQLite("Webshop.db");
-            string text = "Bob";
-            int value = 3;
-            SQLet.Execute($@"INSERT INTO Log (Message,Type,Date) 
-                          Values('{text}',{value},'{DateTime.Now}');");
+            //string text = "Bob";
+            //int value = rnd.Next(1,10);
+            //SQLet.Execute($@"INSERT INTO Log (Message,Type,Date) 
+            //              Values('{text}',{value},'{DateTime.Now}');");
 
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            //Console.WriteLine("Hello World!");
+            //Console.ReadKey();
+            string[] names = new string[] { "Benjamin", "Peter", "Jack", "Mads", "Simon" };
+
+            //string besked = "Hey {0}. The clock is {1}.";
+            string sql = "Insert into Log ({0},{2},{4}) Values('{1}',{3},'{5}')";
+            string[] columns = new string[] { "Message", "Type", "Date" };
+
+            foreach (string name in names)
+            {
+                //Console.WriteLine(string.Format(besked, name, DateTime.Now));
+                //Console.WriteLine(sql, columns[0], name, columns[1], rnd.Next(1,7), columns[2], DateTime.Now);
+                SQLet.Execute(String.Format(sql, columns[0], name, columns[1], rnd.Next(1, 7), columns[2], DateTime.Now));
+            }
+        
+        
         }
     }
 }
