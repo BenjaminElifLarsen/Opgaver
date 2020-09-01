3c9a5ffc8fe304718b5bd5b2666b991b8f22643c
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-09-01 08:17:44 +0200

    Added a Input class for key input. Not fully sure if I want to change some things, e.g. ConsoleKey to ConsoleKeyInfo

---
+++ LagerSystem/Input.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LagerSystem
+{
+    static public class Input
+    {
+
+
+        public static void RunInput()
+        {
+            Thread inputThread = new Thread(InputRun);
+        }
+
+        static private void InputRun()
+        {
+            do
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKey key = Console.ReadKey(true).Key;
+                    Publisher.PubKey.PressKey(key);
+                    Support.BufferFlush();
+                }
+
+            } while (true);
+
+
+        }
+
+    }
+}
