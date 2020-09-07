f142d2ec64dcacb29a73fc80f2dca95a447268ff
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-09-07 09:32:19 +0200

    Added project files and part of the skeleton

---
+++ OOB/ChatApp/Message.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatApp
+{
+    static class Message
+    {
+        static public void AddMessage()
+        {
+
+        }
+
+        static public void RemoveMessage()
+        {
+
+        }
+
+        static public void SeeMessage()
+        {
+
+        }
+
+    }
+
+
+}
---
+++ OOB/ChatApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ChatApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+        }
+
+        static public void Run()
+        {
+            string[] options = new string[] { "Add Message", "Remove Message", "See Message(s)", "Exit" };
+            do
+            {
+                byte selected = Menu.MenuRun(options);
+                switch (selected)
+                {
+                    case 0:
+                        Message.AddMessage();
+                    break;
+
+                    case 1:
+                        Message.RemoveMessage();
+                    break;
+
+                    case 2:
+                        Message.SeeMessage();
+                    break;
+
+                    case 3:
+                        Environment.Exit(0);
+                    break;
+                }
+            } while (true);
+        }
+    }
+}
