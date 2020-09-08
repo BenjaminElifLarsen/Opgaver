c6ebdeac6d4e8f0a30e993e61402356f26fd2443
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-09-08 11:13:42 +0200

    Webshop 2.0

---
+++ DatabaseProgrammering/Webshop_New/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using TECHCOOL;
+
+namespace Webshop_New
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SQLet.ConnectSqlServer("WebShop_New", "BENJAMIN-ELIF-L\\MSSQLSERVER02");
+            ShowBasket();
+        }
+
+        static void ShowBasket()
+        {
+            string[][] result = SQLet.GetArray("Select * From Basket");
+            Console.WriteLine("ID \t Quantity \t ItemID \t CustomerID");
+            foreach(string[] row in result)
+            {
+                foreach(string key in row)
+                {
+                    Console.Write(key + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
