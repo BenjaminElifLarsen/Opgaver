de640ce874ddacc56fbfda74b52b3fc739df6c55
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-09-08 13:12:46 +0200

    Added some changes to the SQLControl

--- OOB/ChatApp/SQLControl.cs
+++ OOB/ChatApp/SQLControl.cs
@@ -28,19 +28,21 @@ namespace ChatApp
                     Use {GetDatabaseName}
                     iF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES Where Table_Type='BASE Table' And Table_Name='User_Information')
                     Begin
-                    Create Table User_Information
-                    (UserID Int Not Null Primary Key Identity(1,1), UserName NVARCHAR(16) Not Null Unique,
-                     UserPassword NVARCHAR(24) Not null, Admin_level Int null);
+                        Create Table User_Information
+                        (UserID Int Not Null Primary Key Identity(1,1), 
+                         UserName NVARCHAR(16) Not Null Unique,
+                         UserPassword NVARCHAR(24) Not null, Admin_level Int null);
                     End
                     iF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES Where Table_Type='BASE Table' And Table_Name='Message_Information')
                     BEGIN
-                     Create Table Message_Information
-                    (MessageID Int Not Null Primary Key IDENTITY(1,1), UserID Int Constraint FK_user_message FOREIGN KEY REFERENCES User_Information(UserID) On Delete Cascade On Update Cascade,
-                     UserName NVARCHAR(16) Not Null FOREIGN KEY REFERENCES User_Information(UserName), 
-                     Message NVARCHAR(255) Not Null, Time NVARCHAR(255) Not Null);
+                         Create Table Message_Information
+                        (MessageID Int Not Null Primary Key IDENTITY(1,1), 
+                         UserID Int Not Null Constraint FK_userID_message FOREIGN KEY REFERENCES User_Information(UserID) On Delete Cascade On Update Cascade,
+                         UserName NVARCHAR(16) Not Null Constraint FK_username_message FOREIGN KEY REFERENCES User_Information(UserName), 
+                         Message NVARCHAR(255) Not Null, Time NVARCHAR(255) Not Null);
 
-                     Insert Into User_Information(UserName,UserPassword,Admin_level)
-                     Values('Benjamin','Test123.',9)
+                         Insert Into User_Information(UserName,UserPassword,Admin_level)
+                         Values('Benjamin','Test123.',9)
                     END
                 End
             ");
