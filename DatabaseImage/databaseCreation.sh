e3b910b4a66259134b12a80901d036a1cb90e912
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-09-08 11:15:22 +0200

    working on a docker image and bash file

---
+++ DatabaseImage/databaseCreation.sh
@@ -0,0 +1,30 @@
+﻿#!/bin/bash
+
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "Password123."
+
+Create Database StorageDB
+Go
+
+Use StorageDB
+Create Table Inventory (ID NVARCHAR(4,16) Not null Primary Key, name NVARCHAR(255) Not null , amount INT Not null , Type NVARCHAR(255) Not null )
+Insert Into Inventory Values
+    ('ID-55t','Test',25, 'Liquid'),
+    ('ID-123q','Toaster',2, 'Electronic'),
+    ('ID-55t2','Superproduct',1, 'Liquid'),
+    ('ID-5q1','FOOF',10, 'Combustible Liquid')
+Go
+
+Create Login ProgramUser with Password='ComputerPassword234.'
+Create User ProgramDatabaseUser For ProgramUser
+Alter role db_datawriter Add Member ProgramDatabaseUser
+Alter role db_datareader Add Member ProgramDatabaseUser
+Go
+
+Use StorageDB
+Select * From Inventory
+Go
+
+exit
+
+Quit
+ 
