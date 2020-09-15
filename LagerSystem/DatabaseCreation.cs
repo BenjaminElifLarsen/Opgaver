bef5adf73bacc77d07d5df2b7f49a041ddead4ae
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-09-15 07:53:05 +0200

    outcommented the code in DatabaseCreation.CreateDockerDatabase()

--- LagerSystem/DatabaseCreation.cs
+++ LagerSystem/DatabaseCreation.cs
@@ -11,9 +11,9 @@ namespace LagerSystem
     {
         public static void CreateDockerDatabase()
         {
-            PowerShell ps = PowerShell.Create(); //needs to either add the database part of the script into the image as a linux bash script or make Powershell call into the SQL after creating the container and run the codes.
+            //PowerShell ps = PowerShell.Create(); //needs to either add the database part of the script into the image as a linux bash script or make Powershell call into the SQL after creating the container and run the codes.
             
-            ps.AddScript("docker run -e \"ACCEPT_EULA = Y\" -e \"SA_PASSWORD = Password123.\" -p 1435:1433 --name SQLStorageSystem -d mcr.microsoft.com/mssql/server:2019-CU5-ubuntu-18.04").Invoke(); //also, will not need the -it 
+            //ps.AddScript("docker run -e \"ACCEPT_EULA = Y\" -e \"SA_PASSWORD = Password123.\" -p 1435:1433 --name SQLStorageSystem -d mcr.microsoft.com/mssql/server:2019-CU5-ubuntu-18.04").Invoke(); //also, will not need the -it 
             //ps = PowerShell.Create();
             //ps.AddScript("Start-Sleep -s 20").Invoke();
             //ps = PowerShell.Create();
