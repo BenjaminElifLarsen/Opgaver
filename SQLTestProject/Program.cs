c82a50b3efceb8e76acbd7f5ee01356f71fa734f
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-09-16 11:06:59 +0200

    test SQL project

---
+++ SQLTestProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace SQLTestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sqlTest = SQLControl.CreateConnectionString();
+            Console.WriteLine(sqlTest);
+            SQLControl.DataBase = "StorageDB";
+            SqlConnection sqlConnection = SQLControl.CreateConnection(sqlTest);
+            SQLControl.SelectWare($"Use {SQLControl.DataBase}; Select * From Inventory");
+            SQLControl.AddWare(new string[] {"ID","name","amount","Type"}, new string[] { "'ID-234q2378'", "'Test'", "10", "'TestType'" });
+            SQLControl.RemoveWare("ID = 'ID-234q237'");
+        }
+    }
+}
