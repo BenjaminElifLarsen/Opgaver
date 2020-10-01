87f720d190c1c17c41d43187ad640aacde58055f
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-10-01 07:59:33 +0200

    file from another project that is causing problems

---
+++ OOB/PraticeProgram/BabyPigman.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PraticeProgram
+{
+    class BabyPigman : Pigman
+    {
+        public BabyPigman(string id, int health, string sound, double[] startLocation) : base(id, health, sound, startLocation)
+        {
+        }
+
+        public override string GenerateSound()
+        {
+            return "5 * 2^-1 = 2.5";
+        }
+    }
+}
