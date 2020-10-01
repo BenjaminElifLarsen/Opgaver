c385a3dea9c9bc9b3db2bb20dfc6c448d2a9b212
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-10-01 10:44:30 +0200

    praticed api asp.net

---
+++ Network/ToDoApi/Models/TodoDTOContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApi.Models
+{
+    public class TodoDTOContext : DbContext
+    {
+        public TodoDTOContext(DbContextOptions<TodoDTOContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TodoItemDTO> TodoItems { get; set; }
+    }
+}
