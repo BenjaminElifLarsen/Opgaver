fbf962dc59056ac760d9c6590cf45f24283b3cb5
Benjamin Larsen <beela93@hotmail.com>
2020-10-03 10:40:41 +0200

    added functions to DatabaseCreation to help preparing the database

--- StorageSystemCore/DatabaseCreation.cs
+++ StorageSystemCore/DatabaseCreation.cs
@@ -8,11 +8,28 @@ namespace SQLCode
 {
     static class DatabaseCreation
     {
-        public static void CreateDatabase()
+        private static void CreateDatabase()
         {
             //have a read me file with mention the line below
             //"docker run -e \"ACCEPT_EULA = Y\" -e \"SA_PASSWORD = Password123.\" -p 1435:1433 --name SQLStorageSystem -d mcr.microsoft.com/mssql/server:2019-CU5-ubuntu-18.04").Invoke(); 
 
+        }
+
+        private static void CreateTable()
+        {
+
+        }
+
+        private static void CreateDefaultEntries()
+        {
+
+        }
+
+        public static void InitialiseDatabase()
+        {
+            CreateDatabase();
+            CreateTable();
+            CreateDefaultEntries();
         }
     }
 }
